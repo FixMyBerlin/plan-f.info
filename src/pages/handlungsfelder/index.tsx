b0a7e4b0f12e18b26359f028c0e0954c250d708b
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-04-24 19:18:13 +0200

    update layout /handlungsfelder

--- src/pages/handlungsfelder/index.tsx
+++ src/pages/handlungsfelder/index.tsx
@@ -1,7 +1,12 @@
+import { PageProps, graphql } from 'gatsby';
 import React from 'react';
-import { Fundings } from '~/components/StartPage';
-import { Hero, Content, HelmetSeo, Breadcrumbs } from '~/components/Layout';
-import { graphql, PageProps, Link } from 'gatsby';
+import { Breadcrumbs, HelmetSeo, Hero } from '~/components/Layout';
+import { Article } from '~/components/Layout/Article';
+import { CardIconAndText } from '~/components/Layout/CardIconAndText';
+import { CardWrapper } from '~/components/Layout/CardWrapper';
+import { Section } from '~/components/Layout/Section';
+import { icons } from '~/components/StartPage';
+import { H2, H3 } from '~/components/Text';
 
 export const query = graphql`
   query TopicTeasers {
@@ -10,6 +15,7 @@ export const query = graphql`
         slug
         name
         description {
+          s
           data {
             description
           }
@@ -22,29 +28,63 @@ export const query = graphql`
 const IndexPage: React.FC<PageProps<Queries.TopicTeasersQuery>> = ({
   data: { topics },
 }) => {
-  const title = 'Handlungsfelder';
+  const title = 'Wissensspeicher';
   return (
     <>
       <HelmetSeo title={title} />
       <Hero title={title}>
         <Breadcrumbs names={[title]} />
       </Hero>
-      <section className="pt-1">
-        <h1>Liste der Handlungsfelder</h1>
-        <Content>
+      <Section>
+        <Article>
+          <p>
+            Kommunikation und Öffentlichkeitsarbeit sind ein zentrales
+            Handlungsfeld der Radverkehrsförderung, welches insbesondere
+            Personen, die (noch) nicht Fahrrad fahren, adressiert und erreichen
+            möchte. Neben Maßnahmen im Bereich #Marketing und Kommunikation
+            können auch #Innovationen und #Modellprojekte einen wichtigen
+            Beitrag dazu leisten, dass das Fahrrad als Mobilitätsmittel beworben
+            und die Bevölkerung zum Radfahren motiviert wird. Darüber hinaus ist
+            es wichtig, dass Kommunen selbst als #Vorbild auftreten und den
+            Radverkehr in der eigenen Verwaltung fördern. Ein positives Framing
+            des Fahrradfahrens durch gute Öffentlichkeitsarbeit zielt darauf ab,
+            das Fahrrad als Alltagsverkehrsmittel zu „normalisieren“.
+            Gleichzeitig ist die Verbreitung wichtiger Informationen, zum
+            Beispiel über radverkehrsbezogene Angebote und Möglichkeiten in
+            Ihrer Kommune, ein wichtiger Baustein, um mehr Menschen das
+            Radfahren zugänglich zu machen. Gute Praxisbeispiele zeigen auf, wie
+            verschiedene Maßnahmen aus dem Handlungsfeld Kommunikation und
+            Öffentlichkeitsarbeit in den Kommunen umgesetzt wurden und dazu
+            beitragen, den Radverkehr zu fördern. Wichtige Leitfäden und
+            Literaturtipps unterstützen Sie in der Umsetzung.
+          </p>
+        </Article>
+      </Section>
+      <Section className="bg-purple-300">
+        <H2>Handlungsfelder</H2>
+        <p>
+          Entdecken Sie die verschiedenen Handlungsfelder. Dort finden Sie
+          Maßnahmen und deren zugehörigen Praxisbeispiele
+        </p>
+
+        <CardWrapper>
           {topics.nodes.map((topic) => (
-            <Link to={topic.slug}>
-              <div className="pt-6">
-                <h1>{topic.name}</h1>
-                {topic.description.data.description}
+            <CardIconAndText
+              link={topic.slug}
+              icon={<icons.governance className="h-24 w-24 flex-shrink-0" />}
+            >
+              <H3>{topic.name}</H3>
+              <div className="line-clamp-4 overflow-hidden">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequatur, inventore beatae rerum, et tempore eum, possimus
+                aliquid laboriosam a id nemo! Architecto, harum maxime
+                necessitatibus tenetur voluptate placeat fuga qui!
+                {/* {topic.description.data.description} */}
               </div>
-            </Link>
+            </CardIconAndText>
           ))}
-        </Content>
-      </section>
-      <div className="object-left pt-28 pb-6">
-        <Fundings />
-      </div>
+        </CardWrapper>
+      </Section>
     </>
   );
 };
