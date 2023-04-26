169b108d4d8051229939fa5435c4dae0e5743ac0
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-04-26 17:24:24 +0200

    update layout page/[massnahme]

--- src/pages/handlungsfelder/{strapiMeasure.topic__slug}/{strapiMeasure.slug}.tsx
+++ src/pages/handlungsfelder/{strapiMeasure.topic__slug}/{strapiMeasure.slug}.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
-import { Fundings } from '~/components/StartPage';
-import { Hero, Content, HelmetSeo, Breadcrumbs } from '~/components/Layout';
-import { graphql, Link, PageProps } from 'gatsby';
+import { PageProps, graphql } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import React from 'react';
+import { Breadcrumbs, HelmetSeo, Hero } from '~/components/Layout';
+import { Article } from '~/components/Layout/Article';
+import { CardImageAndTextResponsive } from '~/components/Layout/CardImageAndTextResponsive';
+import { CardImageAndTextVertical } from '~/components/Layout/CardImageAndTextVertical';
+import { CardWrapperThreeCols } from '~/components/Layout/CardWrapperThreeCols';
+import { Section } from '~/components/Layout/Section';
+import { LinkButtonWithArrow } from '~/components/PageTopic/LinkButtonWithArrow';
+import { H2, H3, P } from '~/components/Text';
+import { Prose } from '~/components/core/Prose';
 
 const MeasureDetails: React.FC<PageProps<Queries.MeasureDetailsQuery>> = ({
   data: { measure },
@@ -10,71 +17,81 @@ const MeasureDetails: React.FC<PageProps<Queries.MeasureDetailsQuery>> = ({
   return (
     <>
       <HelmetSeo title={measure.name} />
-      <Hero title="Handlungsfelder" />
-      <section className="pt-1">
-        <Content>
-          <div className="mt-6 ml-10">
-            <Breadcrumbs
-              names={['Handlungsfelder', measure.topic.name, measure.name]}
-            />
-          </div>
-          <div className="bg-purple-300 pl-2">
-            <div className="bg-white pl-6">
-              <h1 className="mt-6">{measure.name}</h1>
-              <div className="mt-2">{measure.description.data.description}</div>
-            </div>
-          </div>
-          <div className="relative bg-green-500  px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
-            <div className="relative mx-auto max-w-7xl">
-              <div className="text-left">
-                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                  Praxisbeispiele
-                </h2>
-                <p className="mt-3 max-w-2xl text-xl text-gray-500 sm:mt-4">
-                  Schau was andere Kommunen zu diesem Thema bereits für
-                  Maßnahmen umgesetzt haben.
-                </p>
-              </div>
-              <div className="mx-auto mt-12 grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3">
-                {measure.examples.map((example) => (
-                  <div
-                    key={example.title}
-                    className="flex flex-col overflow-hidden rounded-lg shadow-lg"
-                  >
-                    <div className="flex-shrink-0">
-                      {example.image && (
-                        <Link to={example.slug}>
-                          <GatsbyImage
-                            image={getImage(
-                              example.image.image.localFile as any
-                            )}
-                            alt={`Titelbild: ${example.title}`}
-                          />
-                        </Link>
-                      )}
-                    </div>
-                    <div className="flex flex-1 flex-col justify-between bg-white p-6">
-                      <div className="flex-1">
-                        <Link to={example.slug} className="mt-2 block">
-                          <p className="text-xl font-semibold text-gray-900">
-                            {example.title}
-                          </p>
-                          <p className="mt-3 text-base text-gray-500 line-clamp-3">
-                            {example.shortDescription}
-                          </p>
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        </Content>
-      </section>
-      <div className="object-left pt-28 pb-6">
-        <Fundings />
-      </div>
+      <Hero title={measure.name} className="bg-green-500">
+        <Breadcrumbs
+          names={['Wissensspeicher', measure.topic.name, measure.name]}
+        />
+      </Hero>
+      <Section className="flex flex-col gap-10 lg:flex-row">
+        <Article>
+          <Prose>{measure.description.data.description}</Prose>
+        </Article>
+
+        <GatsbyImage
+          className="h-72 w-72 flex-shrink-0"
+          image={getImage(measure.image.image.localFile as any)}
+          alt={`Titelbild ${measure.name}`}
+        />
+      </Section>
+
+      <Section className="mb-20 flex flex-col gap-10 sm:flex-row sm:gap-20">
+        <div className="flex flex-col items-start gap-5">
+          <H3 className="uppercase">Leitfäden</H3>
+          <LinkButtonWithArrow href="/">Leitfaden 1</LinkButtonWithArrow>
+          <LinkButtonWithArrow href="/">Leitfaden 2</LinkButtonWithArrow>
+        </div>
+        <div className="flex flex-col items-start gap-5">
+          <H3 className="uppercase">weitere Hinweise</H3>
+          {measure.additionalResources?.length &&
+            measure.additionalResources.map((resource) => (
+              <LinkButtonWithArrow href={resource.url}>
+                {resource.display}
+              </LinkButtonWithArrow>
+            ))}
+          {/* TODO Warum keine query Ergebnisse? */}
+        </div>
+      </Section>
+
+      <Section className="mb-12 bg-lime-300">
+        <H2>Praxisbeispiele</H2>
+        <P>
+          Schau was andere Kommunen zu diesem Thema bereits für Maßnahmen
+          umgesetzt haben.
+        </P>
+        <CardWrapperThreeCols className="mt-12">
+          {measure.examples.map((example) => (
+            <CardImageAndTextVertical
+              key={example.slug}
+              link={example.slug}
+              image={getImage(example.image.image.localFile as any)}
+            >
+              <H3>{example.title}</H3>
+              <Prose className="line-clamp-4">{example.shortDescription}</Prose>
+            </CardImageAndTextVertical>
+          ))}
+        </CardWrapperThreeCols>
+      </Section>
+
+      <Section className="bg-gray-300">
+        <H2>Weitere Beiträge</H2>
+        {/* TODO ? Was sind die weiteren Beiträge in den Daten?? */}
+        <P>
+          TODO Entdecken Sie die Verschiedenen Maßnahmen, die zu diesem
+          Handlungsfeld gehören, dort finden Sie auch viele Praxisbeispiele
+        </P>
+        <div className="mt-12 flex flex-col gap-5">
+          {measure.examples.map((example) => (
+            <CardImageAndTextResponsive
+              key={example.slug}
+              link={example.slug}
+              image={getImage(example.image.image.localFile as any)}
+            >
+              <H3>{example.title}</H3>
+              <Prose className="line-clamp-4">{example.shortDescription}</Prose>
+            </CardImageAndTextResponsive>
+          ))}
+        </div>
+      </Section>
     </>
   );
 };
@@ -99,6 +116,10 @@ export const query = graphql`
         data {
           description
         }
+      }
+      additionalResources {
+        url
+        display
       }
       topic {
         name
