8043dd4151af17dcc3259ac15ba596228594d629
Tobias <tobias@fixmycity.de>
2023-04-27 14:13:23 +0200

    Footer: Hide facebook b/c no link

--- src/components/Layout/Footer/SocialMediaLinks/SocialMediaLinks.tsx
+++ src/components/Layout/Footer/SocialMediaLinks/SocialMediaLinks.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-
 import { Link } from '~/components/core/links/Link';
-import FacebookIcon from './assets/facebook-icon.svg'; // https://fontawesome.com/icons/twitter?s=brands
+// import FacebookIcon from './assets/facebook-icon.svg'; // https://fontawesome.com/icons/twitter?s=brands
 import GithubIcon from './assets/github-icon.svg';
 import InstagramIcon from './assets/instagram-icon.svg';
 import TwitterIcon from './assets/twitter-icon.svg'; // https://fontawesome.com/icons/twitter?s=brands
@@ -15,9 +14,9 @@ export const SocialMediaLinks: React.FC = () => {
       <Link href="https://www.instagram.com/planf_impulse/">
         <InstagramIcon className="h-5 w-5" />
       </Link>
-      <Link href="https://twitter.com/planf7">
+      {/* <Link href="https://twitter.com/planf7">
         <FacebookIcon className="h-5 w-5" />
-      </Link>
+      </Link> */}
       <Link href="https://github.com/orgs/FixMyBerlin/repositories?q=plan-f&type=all&language=&sort=">
         <GithubIcon className="h-5 w-5" />
       </Link>
