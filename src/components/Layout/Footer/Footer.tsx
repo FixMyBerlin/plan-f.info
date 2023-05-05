7e4476e64b9b4e5a9459b8a90e8e791c5562cab0
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-05-05 14:26:47 +0200

    remove copyright, move social sharing footer

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -9,22 +9,17 @@ export const Footer: React.FC = () => {
   return (
     <>
       <Fundings />
-      <footer
-        className="z-0 flex flex-col justify-between gap-8 bg-black px-10 py-10 text-sm sm:flex-row sm:px-12"
-        aria-labelledby="footer-heading"
-      >
-        <div className="flex flex-col justify-between gap-6 gap-8">
-          <Link href="/">
-            <LogoNegative height={48} width={127} />
-          </Link>
-          <p className="hidden text-sm text-gray-400 sm:block">
-            © 2022 plan GmbH
-          </p>
-        </div>
-        <div className="flex flex-col justify-between gap-6 sm:items-end">
-          <SocialMediaLinks />
-          <FooterLinks />
-          <p className="text-sm text-gray-400 sm:hidden">© 2022 plan GmbH</p>
+      <footer className="z-0 " aria-labelledby="footer-heading">
+        <div className="flex flex-col justify-between gap-8 bg-black px-10 py-10 text-sm sm:flex-row sm:px-12">
+          <div className="flex flex-col justify-between gap-8">
+            <Link href="/">
+              <LogoNegative height={48} width={127} />
+            </Link>
+            <SocialMediaLinks />
+          </div>
+          <div className="flex flex-col justify-end gap-6 sm:items-end">
+            <FooterLinks />
+          </div>
         </div>
       </footer>
     </>
