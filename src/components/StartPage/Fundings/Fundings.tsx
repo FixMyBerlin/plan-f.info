e1297e5bde5015a48cb4ca075896a062a6cdc798
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-05-10 19:14:20 +0200

    remove veloplan logo, update text and sizing Footer

--- src/components/StartPage/Fundings/Fundings.tsx
+++ src/components/StartPage/Fundings/Fundings.tsx
@@ -2,21 +2,19 @@ import React from 'react';
 import { P } from '~/components/Text';
 import BMDVLogo from './assets/bmdv.svg';
 import FairSpacesLogo from './assets/fairspaces.svg';
-import VeloplanLogo from './assets/veloplan.png';
 
 export const Fundings: React.FC = () => {
   return (
     <div className="flex w-full flex-col px-10 py-16 sm:py-24">
-      <div className="mx-auto flex max-w-3xl flex-col sm:flex-row">
-        <div className="flex flex-col">
-          <P>Gefördert durch:</P>
-          <BMDVLogo height="100" />
+      <div className="flex max-w-3xl flex-col sm:flex-row sm:gap-24">
+        <div className="flex flex-col justify-between">
+          <P className="!-mb-6">Gefördert durch:</P>
+          <BMDVLogo width={160} />
         </div>
-        <div className="flex flex-col">
-          <P>Partner von PlanF:</P>
-          <div className="grid grid-cols-2 gap-4">
-            <img src={VeloplanLogo} alt="veloplan logo" />
-            <FairSpacesLogo />
+        <div className="flex flex-col justify-between">
+          <P>Ein Projekt von:</P>
+          <div className="py-6">
+            <FairSpacesLogo width={160} />
           </div>
         </div>
       </div>
