7d8c5b7fa381bbaf74fddfefc22af11e66d60958
Laurenz Rasche <laurenz.rasche@gmail.com>
2023-05-16 16:50:15 +0200

    add missing nl

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 require('dotenv').config({
   path: `.env`,
 });
+
 module.exports = {
   // Allows the use of JSX without having to import React
   // Docs https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/#jsxruntime
