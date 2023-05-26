8c34e28e6a71474e8d12517b142060f79ffa4d14
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-05-26 12:43:49 +0200

    add subtitle prop to Hero

--- src/components/Layout/Hero.tsx
+++ src/components/Layout/Hero.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import React, { ReactNode } from 'react';
-import { H2 } from '../Text';
+import { H2, H3 } from '../Text';
 
 type Props = {
   title: string;
@@ -8,26 +8,31 @@ type Props = {
   breadcrumbs?: ReactNode;
   className?: string;
   bgColor?: string;
+  subtitle?: string;
 };
 
 export const Hero: React.FC<Props> = ({
   breadcrumbs,
   className,
   title,
+  subtitle,
   children,
   bgColor = 'bg-white',
 }) => {
   return (
     <section
       className={clsx(
-        'relative mb-12 flex flex-col justify-center gap-4 rounded-b-3xl px-5 pb-8 pt-36 md:px-10 md:pb-16',
+        'relative mb-16 flex flex-col justify-center gap-4 rounded-b-3xl px-5 pb-8 pt-36 md:px-10 md:pb-16',
         className,
         bgColor
       )}
     >
       {breadcrumbs && <div className="hidden md:block">{breadcrumbs}</div>}
       <div>{children}</div>
-      <H2>{title}</H2>
+      <div>
+        <H2>{title}</H2>
+        {subtitle && <H3>{subtitle}</H3>}
+      </div>
     </section>
   );
 };
