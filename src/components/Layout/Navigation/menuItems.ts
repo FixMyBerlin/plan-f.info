14c2f1ea3813914c7755224b808de403a4ee1152
Tobias <tobias@fixmycity.de>
2023-05-30 12:42:57 +0200

    Update Menu for "weitere Angebote" and add tailing slash

--- src/components/Layout/Navigation/menuItems.ts
+++ src/components/Layout/Navigation/menuItems.ts
@@ -1,12 +1,12 @@
 import { wikiPath } from '~/components/utils';
 
 export const menuItems = {
-  Wissensspeicher: `/${wikiPath}`,
-  'Plan F Check': '/plan-f-check',
-  About: '/about',
-  Artikel: '/artikel',
-  Award: '/award/plan-f-award-2022',
-  'Weitere Angebote': '/weitere-angebote/plan-f-handbuch',
+  Wissensspeicher: `/${wikiPath}/`,
+  'Plan F Check': '/plan-f-check/',
+  About: '/about/',
+  Artikel: '/artikel/',
+  Award: '/award/plan-f-award-2022/',
+  'Weitere Angebote': '/weitere-angebote/',
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -45,8 +45,8 @@ export const menuItemsWithChildren = {
     'Plan F Award 2022': '/award/plan-f-award-2022',
   },
   'Weitere Angebote': {
-    'Plan F Handbuch': '/weitere-angebote/plan-f-handbuch',
-    Audits: '/weitere-angebote/audits',
-    'E-Learning': '/weitere-angebote/e-Learning',
+    'Plan F Handbuch': '/weitere-angebote/handbuch/',
+    Audits: '/weitere-angebote/audits/',
+    'E-Learning': '/weitere-angebote/e-learning/',
   },
 };
