65e1cb0f76e7599ea6a45617b1379b07d7efa7e4
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-05-31 12:11:15 +0200

    update sitemap in Footer

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,13 +1,14 @@
 import clsx from 'clsx';
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Fundings } from '~/components/StartPage';
 import { Link } from '~/components/core/links/Link';
-import { menuItems } from '../Navigation/menuItems';
+import { menuItems, menuItemsWithChildren } from '../Navigation/menuItems';
 import LogoNegative from '../assets/LogoNegative.svg';
 import { FooterLinks } from './FooterLinks/FooterLinks';
 import { SocialMediaLinks } from './SocialMediaLinks/SocialMediaLinks';
 
 export const Footer: React.FC = () => {
+
   return (
     <>
       <Fundings />
@@ -17,15 +18,61 @@ export const Footer: React.FC = () => {
       >
         <ul
           className={clsx(
-            'grid grid-cols-2 gap-6 pb-6 text-sm sm:grid-cols-3 md:flex md:flex-row md:justify-between'
+            'grid grid-cols-2 gap-10 pb-6 text-sm text-gray-400 sm:grid-cols-3 md:flex md:flex-row md:justify-between'
           )}
         >
           {Object.keys(menuItems).map((key) => (
-            <li key={key}>
-              <Link href={menuItems[key]} className="uppercase text-gray-400">
-                {key}
-              </Link>
-            </li>
+            // eslint-disable-next-line react/jsx-no-useless-fragment
+            <Fragment key={key}>
+              {key === 'Wissensspeicher' ? (
+                // Inner Disclosure
+                <div className="flex flex-col gap-3">
+                  {/* First Link: Wissensspeicher */}
+                  <p className="text-sm uppercase text-gray-400">{key}</p>
+                  <Link href={menuItems[key]} className={clsx('!text-sm')}>
+                    Handlungsfelder
+                  </Link>
+                  <Link href="/" className={clsx('!text-sm')}>
+                    Suche
+                  </Link>
+                </div>
+              ) : (
+                // eslint-disable-next-line react/jsx-no-useless-fragment
+                <>
+                  {key in menuItemsWithChildren ? (
+                    <div className="flex flex-col gap-3">
+                      <p className="!text-sm uppercase !text-gray-400">{key}</p>
+                      {/* List of all children */}
+                      {Object.keys(menuItemsWithChildren[key]).map(
+                        (childKey) => (
+                          <li
+                            className="max-w-[150px] list-none !truncate"
+                            key={menuItemsWithChildren[key][childKey]}
+                          >
+                            <Link
+                              href={`${menuItems[key]}${menuItemsWithChildren[key][childKey]}`}
+                              className={clsx(' !text-sm')}
+                            >
+                              {childKey}
+                            </Link>
+                          </li>
+                        )
+                      )}
+                    </div>
+                  ) : (
+                    <div className="flex flex-col gap-3">
+                      <p className="!text-sm uppercase !text-gray-400">{key}</p>
+                      <Link
+                        href={menuItems[key]}
+                        className="!text-sm !text-gray-400"
+                      >
+                        {key}
+                      </Link>
+                    </div>
+                  )}
+                </>
+              )}
+            </Fragment>
           ))}
         </ul>
         <div className="flex flex-col justify-between gap-8 border-t border-gray-400 pt-8 text-sm sm:flex-row">
