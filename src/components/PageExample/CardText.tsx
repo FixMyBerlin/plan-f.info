509a3f144acb5a6aed06eff18fd2a7926e606889
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-06-07 12:08:13 +0200

    update structure TextCard

--- src/components/PageExample/CardText.tsx
+++ src/components/PageExample/CardText.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 import React, { ReactNode } from 'react';
 import { H3 } from '../Text';
-import { Prose } from '../core/Prose';
+import { Prose, planFMarkdownProseClasses } from '../core/Prose';
 
 type Props = {
   title: string;
@@ -19,8 +19,10 @@ export const CardText: React.FC<Props> = ({
     <div className={clsx(className, 'rounded-b-3xl bg-gray-50')}>
       <H3 className="!md:mb-0 !md:mt-0 !mb-0 !mt-0 bg-lime-200 p-4">{title}</H3>
       <div className="p-4">
+        {children && (
+          <div className={planFMarkdownProseClasses}>{children}</div>
+        )}
         {markdownHTML && <Prose markdownHTML={markdownHTML} />}
-        {children && children}
       </div>
     </div>
   );
