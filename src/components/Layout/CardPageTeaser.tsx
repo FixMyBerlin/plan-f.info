881b6adfdfcc7cdce85415572d322af6a3bcf175
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-06-16 13:52:47 +0200

    update style CardPageTeaser

--- src/components/Layout/CardPageTeaser.tsx
+++ src/components/Layout/CardPageTeaser.tsx
@@ -1,7 +1,7 @@
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import clsx from 'clsx';
 import React, { ReactNode } from 'react';
-import { H2 } from '../Text';
+import { H2, P } from '../Text';
 import { Link } from '../core/links';
 
 type Props = {
@@ -20,12 +20,8 @@ export const PageTeaserCard: React.FC<Props> = ({
   return (
     <section className={clsx('bg-gray-50 p-5', className)}>
       <H2 className="!mt-0">{title}</H2>
-      <div>{children}</div>
-      <Link
-        className="mt-10 flex items-center gap-2"
-        button="black"
-        href={link}
-      >
+      <P>{children}</P>
+      <Link className="mt-6 flex items-center gap-2" button="black" href={link}>
         <ArrowRightIcon className="h-5 w-5 " />
         Weiterlesen
       </Link>
