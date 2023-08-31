f3bc131ef155bc85f1c61c73d190972227890726
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-08-31 18:01:37 +0200

    use RadioButtonGroup from headlessUI

--- src/pages/fahrradcheck/fragebogen.tsx
+++ src/pages/fahrradcheck/fragebogen.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/jsx-fragments */
+import { RadioGroup } from '@headlessui/react';
+import clsx from 'clsx';
 import { HeadFC } from 'gatsby';
 import { Fragment } from 'react';
 import { questionBlocks } from '~/components/Fahrradcheck';
@@ -19,17 +21,42 @@ const FahrradcheckPage = () => {
           {questionBlocks.map((block) => (
             <Fragment key={block.title}>
               <h2>{block.title}</h2>
-              {block.questions.map((question) => (
+              {block.questions.map((question, i) => (
                 <Fragment key={question.question}>
                   <h3>{question.question}</h3>
-                  <ul>
-                    {question.options.map((option) => (
-                      <li key={option.text}>
-                        <span>{option.weight}</span> -{' '}
-                        <span>{option.text}</span>
-                      </li>
-                    ))}
-                  </ul>
+
+                  <RadioGroup
+                    value={String(i)}
+                    onChange={() => console.log('success')}
+                    className="mt-2"
+                  >
+                    <div className="grid grid-cols-3 gap-3 sm:grid-cols-5">
+                      {question.options.map((option) => (
+                        <RadioGroup.Option
+                          key={option.text}
+                          value={option.weight}
+                          className={({ active, checked }) =>
+                            clsx(
+                              active
+                                ? 'ring-2 ring-indigo-600 ring-offset-2'
+                                : '',
+                              checked
+                                ? 'bg-indigo-600 text-white hover:bg-indigo-500'
+                                : 'ring-1 ring-inset ring-gray-300 bg-white text-gray-900 hover:bg-gray-50',
+                              'flex items-center justify-center rounded-md py-3 px-3 text-sm font-semibold uppercase sm:flex-1',
+                            )
+                          }
+                        >
+                          <RadioGroup.Label
+                            className="lowercase whitespace-nowrap"
+                            as="span"
+                          >
+                            {option.text}
+                          </RadioGroup.Label>
+                        </RadioGroup.Option>
+                      ))}
+                    </div>
+                  </RadioGroup>
                 </Fragment>
               ))}
             </Fragment>
