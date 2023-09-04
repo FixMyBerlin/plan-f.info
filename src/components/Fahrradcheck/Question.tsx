76c9ef828b1f052d0260b62da0cc3f42529b3247
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-09-04 11:06:38 +0200

    extract QuestionItem

---
+++ src/components/Fahrradcheck/Question.tsx
@@ -0,0 +1,56 @@
+import { RadioGroup } from '@headlessui/react';
+import clsx from 'clsx';
+import React, { useEffect, useState } from 'react';
+import { Question } from './questions.const';
+
+type Props = {
+  question: Question;
+};
+
+const QuestionItem: React.FC<Props> = ({ question }) => {
+  const [questionResult, setQuestionResult] = useState(null);
+  useEffect(() => {
+    console.log(questionResult);
+  }, [questionResult]);
+
+  return (
+    <>
+      <h3>
+        {question.id + 1}. {question.question}
+      </h3>
+
+      <RadioGroup
+        value={questionResult}
+        onChange={setQuestionResult}
+        className="mt-2"
+      >
+        <div className="grid grid-cols-3 gap-3 sm:grid-cols-5">
+          {question.options.map((option) => (
+            <RadioGroup.Option
+              key={option.text}
+              value={option.weight}
+              className={({ active, checked }) =>
+                clsx(
+                  active ? 'ring-2 ring-indigo-600 ring-offset-2' : '',
+                  checked
+                    ? 'bg-gray-200 hover:bg-gray-500'
+                    : 'ring-1 ring-inset ring-gray-300 bg-white text-gray-900 hover:bg-gray-50',
+                  'flex items-center justify-center rounded-md py-3 px-3 text-sm font-semibold uppercase sm:flex-1',
+                )
+              }
+            >
+              <RadioGroup.Label
+                className="lowercase whitespace-nowrap"
+                as="span"
+              >
+                {option.text}
+              </RadioGroup.Label>
+            </RadioGroup.Option>
+          ))}
+        </div>
+      </RadioGroup>
+    </>
+  );
+};
+
+export default QuestionItem;
--- src/pages/fahrradcheck/fragebogen.tsx
+++ src/pages/fahrradcheck/fragebogen.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/jsx-fragments */
-import { RadioGroup } from '@headlessui/react';
-import clsx from 'clsx';
 import { HeadFC } from 'gatsby';
 import { Fragment } from 'react';
 import { questionBlocks } from '~/components/Fahrradcheck';
+import QuestionItem from '~/components/Fahrradcheck/Question';
 import { Content, Hero, MetaTags } from '~/components/Layout';
 import { Section } from '~/components/Layout/Section';
 import { articleProseClasses } from '~/components/core/articleProseClasses';
@@ -21,43 +20,8 @@ const FahrradcheckPage = () => {
           {questionBlocks.map((block) => (
             <Fragment key={block.title}>
               <h2>{block.title}</h2>
-              {block.questions.map((question, i) => (
-                <Fragment key={question.question}>
-                  <h3>{question.question}</h3>
-
-                  <RadioGroup
-                    value={String(i)}
-                    onChange={() => console.log('success')}
-                    className="mt-2"
-                  >
-                    <div className="grid grid-cols-3 gap-3 sm:grid-cols-5">
-                      {question.options.map((option) => (
-                        <RadioGroup.Option
-                          key={option.text}
-                          value={option.weight}
-                          className={({ active, checked }) =>
-                            clsx(
-                              active
-                                ? 'ring-2 ring-indigo-600 ring-offset-2'
-                                : '',
-                              checked
-                                ? 'bg-indigo-600 text-white hover:bg-indigo-500'
-                                : 'ring-1 ring-inset ring-gray-300 bg-white text-gray-900 hover:bg-gray-50',
-                              'flex items-center justify-center rounded-md py-3 px-3 text-sm font-semibold uppercase sm:flex-1',
-                            )
-                          }
-                        >
-                          <RadioGroup.Label
-                            className="lowercase whitespace-nowrap"
-                            as="span"
-                          >
-                            {option.text}
-                          </RadioGroup.Label>
-                        </RadioGroup.Option>
-                      ))}
-                    </div>
-                  </RadioGroup>
-                </Fragment>
+              {block.questions.map((question) => (
+                <QuestionItem key={question.id} question={question} />
               ))}
             </Fragment>
           ))}
