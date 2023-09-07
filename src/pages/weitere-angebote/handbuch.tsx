311872d03acc485fcfcc2a428060b65c8aec833a
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-09-07 10:33:01 +0200

    update /handbuch page, add PDF

--- src/pages/weitere-angebote/handbuch.tsx
+++ src/pages/weitere-angebote/handbuch.tsx
@@ -4,7 +4,8 @@ import { Content, MetaTags, Hero } from '~/components/Layout';
 import { ImageWithCopyright } from '~/components/Layout/ImageWithCopyright';
 import { Section } from '~/components/Layout/Section';
 import { articleProseClasses } from '~/components/core/articleProseClasses';
-import { MailLink } from '~/components/core/links';
+import { Link, MailLink } from '~/components/core/links';
+import manualPdf from './PlanF-Handbuch_2023.pdf';
 
 const title = 'Plan F Handbuch';
 
@@ -19,23 +20,32 @@ const ManualPage = () => {
             Angebote komprimiert und übersichtlich zusammengefasst.
           </p>
           <p>
-            Die ersten 200 Exemplare können kostenlos bestellt werden. Hiernach
-            kostet es 25 € / Exemplar.
+            Das Handbuch kann als Hardcopy bestellt oder als PDF heruntergeladen
+            werden.
           </p>
-          <MailLink
-            className="mt-4"
-            button
-            mailto="kontakt@plan-f.info"
-            subject="Bestellung Handbuch"
-            body={`Vorname, Name
+          <p>
+            Die ersten 200 gedruckten Exemplare können kostenlos bestellt
+            werden. Hiernach kostet es 25 € / Exemplar.
+          </p>
+
+          <div className="mt-8 flex flex-col sm:flex-row gap-6 items-start">
+            <MailLink
+              button
+              mailto="kontakt@plan-f.info"
+              subject="Bestellung Handbuch"
+              body={`Vorname, Name
             Adresse
             Gewünschte Anzahl
             optional: E-Mail Adresse und Telefonnummer für Rückfragen
             `}
-            type="button"
-          >
-            Plan F Handbuch bestellen
-          </MailLink>
+              type="button"
+            >
+              Plan F Handbuch (Hardcopy) bestellen
+            </MailLink>
+            <Link button href={manualPdf}>
+              PDF herunterladen
+            </Link>
+          </div>
           <ImageWithCopyright copyright="Plan F Handbuch">
             <StaticImage
               src="./../../components/WeitereAngebotePages/assets/MockUp_Handbuch.jpg"
