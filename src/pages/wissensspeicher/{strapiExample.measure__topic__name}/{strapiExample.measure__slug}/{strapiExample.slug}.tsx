dc2a3b47f611468df90d6d0ea8dc08297d3f6187
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-09-18 10:38:07 +0200

    Revert "Revert "fix links""
    This reverts 1f9683d

--- src/pages/wissensspeicher/{strapiExample.measure__topic__name}/{strapiExample.measure__slug}/{strapiExample.slug}.tsx
+++ src/pages/wissensspeicher/{strapiExample.measure__topic__name}/{strapiExample.measure__slug}/{strapiExample.slug}.tsx
@@ -119,8 +119,8 @@ const ExampleDetails: React.FC<PageProps<Queries.ExampleDetailsQuery>> = ({
                                   Informationen zu den Daten finden Sie unter
                                   BBSR – Raumbeobachtung – Laufende
                                   Raumbeobachtung – Raumabgrenzungen (
-                                  <Link href="https://verwaltung.bund.de/">
-                                    bund
+                                  <Link href="https://www.bbsr.bund.de/BBSR/DE/forschung/raumbeobachtung/Raumabgrenzungen/deutschland/gemeinden/Raumtypen2010_vbg/Raumtypen2010_LageSied.html;jsessionid=8D851F8ECAFACA91FD6F226E7E7EC3CF.live11312">
+                                    BBSR
                                   </Link>
                                   ).
                                 </p>
